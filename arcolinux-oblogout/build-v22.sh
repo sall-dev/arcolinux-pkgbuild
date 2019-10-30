08c0fe55ab64f23937cba9e46a740a9254bd7703
Erik Dubois <erik.dubois@gmail.com>
2019-10-30 08:56:42 +0100

    update

--- arcolinux-oblogout/build-v22.sh
+++ arcolinux-oblogout/build-v22.sh
@@ -15,7 +15,8 @@ echo "2. makepkg"
 echo "1. chroot"
 echo "0. Do nothing"
 echo "Type the number..."
-read CHOICE
+#read CHOICE
+CHOICE=1
 
 case $CHOICE in
 
@@ -91,6 +92,7 @@ find .   -not -name Makefile\
 		 -not -name '*.jpg'\
 		 -not -name '*.cfg'\
 		 -not -name '*.desktop'\
+     -not -name 'arco*'\
 		 -delete
 
 
