8e0b2dbfbd13248e95ed3f484c934fade25b968f
Erik Dubois <erik.dubois@gmail.com>
2019-11-17 07:14:06 +0100

    update

---
+++ ARCOLINUX/arcolinux-lightdm-gtk-greeter-settings-1.2.2_4/build-v23+.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
+
+destination1="/home/erik/ARCOLINUX-REPO/arcolinux_repo/x86_64/"
+destination2="/home/erik/ARCOLINUX-REPO/arcolinux_repo_3party/x86_64/"
+destination3="/home/erik/ARCOLINUX-REPO/arcolinux_repo_iso/x86_64/"
+destination4="/home/erik/ARCOLINUX-REPO/arcolinux_repo_testing/x86_64/"
+
+destiny=$destination1
+
+# 2. makepkg"
+# 1. chroot"
+
+CHOICE=2
+
+search=arcolinux
+
+tput setaf 2
+echo "#############################################################################################"
+echo "#########        Let us build the package "$(basename `pwd`)
+echo "#############################################################################################"
+tput sgr0
+
+case $CHOICE in
+
+    2 )
+    makepkg --sign
+    ;;
+
+    1 )
+    CHROOT=$HOME/Documents/chroot-arcolinux
+    arch-nspawn $CHROOT/root pacman -Syu
+    makechrootpkg -c -r $CHROOT
+
+    echo "Signing the package"
+    echo "#############################################################################################"
+    gpg --detach-sign $search*pkg.tar.xz
+
+    ;;
+esac
+
+echo "Moving created files to " $destiny
+echo "#############################################################################################"
+mv $search*pkg.tar.xz $destiny
+mv $search*pkg.tar.xz.sig $destiny
+echo "Cleaning up"
+echo "#############################################################################################"
+echo "deleting unnecessary folders"
+echo "#############################################################################################"
+
+rm -rf pkg src
+
+echo "deleting unnecessary files"
+echo "#############################################################################################"
+
+find .   -not -name Makefile\
+		 -not -name readme.install\
+		 -not -name 'PKGBUILD'\
+		 -not -name 'MKPKG'\
+		 -not -name 'LICENCE.md'\
+		 -not -name 'install'\
+		 -not -name '*.prf'\
+		 -not -name '*.hook'\
+		 -not -name '*.conf'\
+		 -not -name 'LICENSE'\
+		 -not -name 'build-v*'\
+		 -not -name 'modprobe.d*'\
+		 -not -name '*.install'\
+		 -not -name '*.patch'\
+		 -not -name '*.png'\
+		 -not -name '*.jpg'\
+		 -not -name '*.cfg'\
+		 -not -name '*.desktop'\
+		 -delete
+
+tput setaf 8
+echo "#############################################################################################"
+echo "###################                       build done                   ######################"
+echo "#############################################################################################"
+tput sgr0
